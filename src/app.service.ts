ee9fd88de22454a659896b17d24b7fc05660324e
astradae <astradaezequiel.tpi@gmail.com>
2022-02-15 16:52:47 -0300

    Injecting Repositories into Providers, Repository API & querying

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  getAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  async getOneById(id: number): Promise<User> {
+    try {
+      const user = await this.userRepository.findOneOrFail(id);
+      return user;
+    } catch (err) {
+      // handle error
+      throw err;
+    }
+  }
+
+  createUser(name: string): Promise<User> {
+    const newUser = this.userRepository.create({ name });
+
+    return this.userRepository.save(newUser);
+  }
+
+  async updateUser(id: number, name: string): Promise<User> {
+    const user = await this.getOneById(id);
+
+    user.name = name;
+
+    return this.userRepository.save(user);
+  }
+
+  async deleteUser(id: number): Promise<User> {
+    const user = await this.getOneById(id);
+
+    return this.userRepository.remove(user);
+
+    return user;
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
--- src/user.entity.ts
+++ src/user.entity.ts
@@ -1,13 +1,10 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class user {
+export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
   name: string;
-
-  @Column()
-  something: string;
 }
